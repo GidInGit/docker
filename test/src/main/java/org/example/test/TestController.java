d49a1b979dd51f218e128c8997be52ede480d796
daniilnislaev <nislaevdan@mail.ru>
2024-07-10 20:07:30 +0300

    third commit

---
+++ test/src/main/java/org/example/test/TestController.java
@@ -0,0 +1,12 @@
+package org.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test-api")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Hello From TestController";
+    }
+}
