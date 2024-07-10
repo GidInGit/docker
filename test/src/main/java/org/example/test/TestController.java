fa7e732d54e7c73d3ef2f633508aa309452a5723
daniilnislaev <nislaevdan@mail.ru>
2024-07-10 20:34:02 +0300

    six commit

--- test/src/main/java/org/example/test/TestController.java
+++ test/src/main/java/org/example/test/TestController.java
@@ -1,9 +1,11 @@
 package org.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test-api")
+@RestController()
+@RequestMapping("/test-api")
 public class TestController {
     @GetMapping
     public String test() {
